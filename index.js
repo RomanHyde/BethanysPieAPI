1bb92d693a89c2042508c5adfe1f802759176eae
RomanHyde <romanhyde1@gmail.com>
2021-06-18 17:11:10 +0100

    add file reading function

--- index.js
+++ index.js
@@ -2,20 +2,25 @@
 // The require() function resolves libraries and modules in the Node search path (ususally \node_modules)
 let express = require('express');
 let app = express();
+let pieRepo = require('./repos/pieRepo')
 
 // Use the express Router object
 let router = express.Router();
-// Pies array for pie objects
-let pies = [
-    { "id" : 1, "name" : "Apple"},
-    { "id" : 2, "name" : "Cherry"},
-    { "id" : 3, "name" : "Peach"}
-];
 
 // Create GET to return a list of all pies
 // request, response and then next for middleware handiling
 router.get('/', (req, res, next) => {
-    res.status(200).send(pies);
+    pieRepo.get((data) => {
+        // Passing a json object through the .json() method
+        res.status(200).json({
+            "status" : 200,
+            "statusTest": "OK",
+            "message": "All pies retrieved!",
+            "data": data
+        });
+    }, (err) => {
+        next(err);
+    });
 });
 
 // Configure router so all the routes are prefixed with /api/v1 e.g http:localhost:5000/api/
