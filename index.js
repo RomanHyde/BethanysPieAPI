39cb198ee13be494f85403a5809d3d409adb5a28
RomanHyde <romanhyde1@gmail.com>
2021-06-18 16:40:41 +0100

    append status code for index.js

--- index.js
+++ index.js
@@ -5,11 +5,17 @@ let app = express();
 
 // Use the express Router object
 let router = express.Router();
+// Pies array for pie objects
+let pies = [
+    { "id" : 1, "name" : "Apple"},
+    { "id" : 2, "name" : "Cherry"},
+    { "id" : 3, "name" : "Peach"}
+];
 
 // Create GET to return a list of all pies
 // request, response and then next for middleware handiling
 router.get('/', (req, res, next) => {
-    res.send("Cherry Pie");
+    res.status(200).send(pies);
 });
 
 // Configure router so all the routes are prefixed with /api/v1 e.g http:localhost:5000/api/
