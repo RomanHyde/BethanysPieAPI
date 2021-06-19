81c6020652698f2ee207c78233a64f2e57551278
RomanHyde <romanhyde1@gmail.com>
2021-06-19 10:22:43 +0100

    configured error module

---
+++ helpers/errorHelpers.js
@@ -0,0 +1,50 @@
+let errorHelpers = {
+    logErrorsToConsole: (err, req, res, next) => {
+        // send error ingo to console.error
+        console.error(`Log Entry: ${JSON.stringify(errorHelpers.errorBuilder(err))}`) //diff
+        console.error("*".repeat(80));
+        // forward err to next middleware
+        next(err)
+    },
+    clientErrorHandler: (err, req, res, next) => {
+        // Check request object for error
+        if (req.xhr) {
+            res.status(500).json({
+                "status": 500,
+                "statusText": "Internal Server Error",
+                "message": "XMLHttpRequest error",
+                "error": {
+                    "errno": 0,
+                    "call": "XMLHttpRequest Call",
+                    "code": "INTERNAL_SERVER_ERROR",
+                    "message": "XMLHttpRequest error"
+                }
+            });
+        } else {
+            // forward err to next middleware
+            next(err);
+        }
+    },
+    // Last in the chain, no next function contained
+    errorHandler: (err, req, res, next) => {
+        res.status(500).json(errorHelpers.errorBuilder(err));
+    },
+    
+    // Configure errorBuilder
+    // moved from index.js to errorHelpers.js
+    errorBuilder: (err) => {
+        return {
+            "status": 500,
+            "statusText": "Internal Server Error",
+            "message": err.message,
+            "error": {
+                "errno": err.errno,
+                "call": err.syscall,
+                "code": "INTERNAL_SERVER_ERROR",
+                "message": err.message
+            }
+        };
+    }
+};
+
+module.exports = errorHelpers;
